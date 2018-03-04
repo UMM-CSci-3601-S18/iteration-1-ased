22276df52166aac7c8730f5e23d104a1008dd148
blake376 <blake376@morris.umn.edu>
2018-03-04 16:11:26 -0600

    Removed User from Main
    user is useful for development, but we probs don't need it it main

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,8 +7,7 @@ import { MATERIAL_COMPATIBILITY_MODE } from '@angular/material';
 import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
-import {UserListComponent} from './users/user-list.component';
-import {UserListService} from './users/user-list.service';
+
 import {EmojiSelectorService} from "./emojis/emoji-selector.service";
 import {EmojiSelectorComponent} from './emojis/emoji-selector.component';
 import {EmojiSelectorResponseDialog} from './emojis/emoji-selector.component';
@@ -16,7 +15,7 @@ import {Routing} from './app.routes';
 import {APP_BASE_HREF} from '@angular/common';
 
 import {CustomModule} from './custom.module';
-import {AddUserComponent} from "./users/add-user.component";
+
 
 
 @NgModule({
@@ -29,19 +28,18 @@ import {AddUserComponent} from "./users/add-user.component";
     declarations: [
         AppComponent,
         HomeComponent,
-        UserListComponent,
-        AddUserComponent,
+
         EmojiSelectorComponent,
         EmojiSelectorResponseDialog
     ],
     providers: [
-        UserListService,
+
         EmojiSelectorService,
         {provide: APP_BASE_HREF, useValue: '/'},
         {provide: MATERIAL_COMPATIBILITY_MODE, useValue: true}
     ],
     entryComponents: [
-        AddUserComponent,
+
         EmojiSelectorResponseDialog,
     ],
     bootstrap: [AppComponent]
