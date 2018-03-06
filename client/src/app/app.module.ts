eaeba31d5db74cecfd5ce9b1d7e80d0e8cc9b28c
monjo003 <monjo003@morris.umn.edu>
2018-03-05 20:19:09 -0600

    I found this on the internet it can be used as enxample. but for some reason is is not working properly. I will try working on it some other time

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,13 +7,21 @@ import { MATERIAL_COMPATIBILITY_MODE } from '@angular/material';
 import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
+
+
 import {UserListComponent} from './users/user-list.component';
 import {UserListService} from './users/user-list.service';
+//import {GoalListComponent} from'./goals/goal-list.component';
+//import {GoalListService} from'./goals/goal-list.service'
+
+
 import {Routing} from './app.routes';
 import {APP_BASE_HREF} from '@angular/common';
 
 import {CustomModule} from './custom.module';
 import {AddUserComponent} from './users/add-user.component';
+
+//import{AddGoalComponents} from'./goals/add-goal.component';
 
 
 @NgModule({
